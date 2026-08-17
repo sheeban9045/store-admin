b3c9806f4768a1d1472e07623587f706726f2bef
sheeban hasan <sheeban.questwalk@gmail.com>
2026-08-17 16:41:54 +0530

    worked on cron

--- app/Controllers/Orders.php
+++ app/Controllers/Orders.php
@@ -1867,17 +1867,6 @@ class Orders extends Security_Controller {
         return $this->template->rander("/orders/renew_success_page", $view_data);
     }
 
-    public function expire_overdue_monthly_plans() {
-        die("This function is disabled for now. Please enable it if you want to use it.");
-        $expiry_check_last_run_date = get_setting("expiry_check_last_run_date");
-        if (!($expiry_check_last_run_date == "" || ($expiry_check_last_run_date != $this->today))) {
-            return false;
-        }
-
-        $this->ci->Orders_model->expire_overdue_monthly_plans();
-
-        $this->ci->Settings_model->save_setting("expiry_check_last_run_date", $this->today);
-    }
 }
 
 /* End of file orders.php */
